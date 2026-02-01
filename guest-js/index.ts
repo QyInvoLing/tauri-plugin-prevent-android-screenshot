8116c52bb7c2a0394d2579d580fcd1e92a060611
QyInvoLing <2258191452@qq.com>
2026-02-01 19:21:24 +0800

    feat(ks-handle): Add JavaScript entrypoint for webview.

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -1,9 +1,15 @@
 import { invoke } from '@tauri-apps/api/core'
 
-export async function ping(value: string): Promise<string | null> {
-  return await invoke<{value?: string}>('plugin:tauri-prevent-android-screenshot|ping', {
+export async function disable_screen_capture(): Promise<null> {
+  return await invoke<{}>('plugin:tauri-prevent-android-screenshot|disable_screen_capture', {
     payload: {
-      value,
     },
-  }).then((r) => (r.value ? r.value : null));
+  }).then(() => null);
+}
+
+export async function enable_screen_capture(): Promise<null> {
+  return await invoke<{}>('plugin:tauri-prevent-android-screenshot|enable_screen_capture', {
+    payload: {
+    },
+  }).then(() => null);
 }
